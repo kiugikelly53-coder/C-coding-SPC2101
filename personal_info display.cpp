bfa97dc72312291518ce4bc55c406c0eda25db01
kiugikelly53-coder <kiugikelly53@gmail.com>
2025-09-22 20:47:26 +0300

    Add files via upload

---
+++ personal_info display.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/*KELLY KIUGI
+CT101/G/26503/25
+COMPUTER SCIENCE
+GROUP A
+description-displaying personal info
+*/
+int main(){
+	
+
+	printf("UNITS THIS SEMESTER \n" );
+	printf("1.Analogue elecronics \n");
+	printf("2.Introduction to computer architecter \n");
+	printf("3.Fundamenals of ICT \n");
+	printf("4.Introduction to programing and problem solving \n ");
+	printf("5.Phyciscs \n");
+	printf("6.Discrete mathematics \n");
+	printf("7.Mathematics of science \n");
+	printf("8.Communication skills \n");
+	printf("STUDENTS HEIGHT \n");
+	printf("181cm \n");
+	printf("STUDENT BANK BALANCE \n" );
+	printf("eighteen thousand shillings \n");
+	printf("STUDENT  PHONE NUMBER \n")
+	;int phone_no = 719732582;
+	printf("phone_number is:%d",phone_no);
+
+	
+	
+	return 0;
+}
