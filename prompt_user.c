f5744b1cfd838f21e67c3fd47f2900f5b698f5f3
kiugikelly53-coder <kiugikelly53@gmail.com>
2025-09-25 09:54:25 +0300

    Add files via upload

---
+++ prompt_user.c
@@ -0,0 +1,20 @@
+/*
+NAME-KELLY KIUGI
+REG NO-CT101/G/26503/25
+COURSE-COMPUTER SCIENCE,group A
+DISCRIPTION-prompt for user info
+ 
+*/
+#include<stdio.h>
+int main(){
+	float height,bank_balance,phone_no;
+	printf("enter your heigtin cm :");
+	scanf("%f",& height);
+	printf("enter your bank balance in ksh:");
+	scanf("%f",& bank_balance);
+	printf("\n enter your phone number:");
+	scanf("%f",& phone_no);
+	
+	return 0;
+	
+}
